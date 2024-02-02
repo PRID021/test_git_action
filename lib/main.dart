d6dc35bb30331e8b8e2617b545bfc2f94c760bed
DucHoang-UIT <hoang.pham@executionlab.asia>
2024-02-02 13:15:26 +0700

    demo distribution

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:test_app/firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
