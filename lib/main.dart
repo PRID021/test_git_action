70ebe5180a00297d64c090d2137781952677a81c
DucHoang-UIT <hoang.pham@executionlab.asia>
2024-02-03 14:21:10 +0700

    edit work flow

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:test_app/firebase_options.dart';
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
